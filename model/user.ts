ff3f70c21c04791bd59f0a86036325d6ee611e22
Amarish Singh Sajwan <amarish.sajwan@gmail.com>
2024-06-28 15:45:46 +0530

    Initial commit

---
+++ model/user.ts
@@ -0,0 +1,39 @@
+import {
+  DataTypes,
+  Model,
+  InferAttributes,
+  InferCreationAttributes,
+  CreationOptional,
+} from "sequelize";
+
+import sequelize from "../db/database";
+
+class User extends Model<InferAttributes<User>, InferCreationAttributes<User>> {
+  declare id: CreationOptional<number>;
+  declare username: string;
+  declare password: string;
+}
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    tableName: "users",
+  }
+);
+
+export { User };
